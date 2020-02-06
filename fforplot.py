7e9e17209d7843955c5e69a51a77c478ab848ac2
GayaneKa <60619757+GayaneKa@users.noreply.github.com>
2020-02-06 04:28:39 -0600

    made changes

--- fforplot.py
+++ fforplot.py
@@ -11,9 +11,9 @@ def hourlyplot(fig,ax1,arag,nora,yere,i,lim):
     ax1.bar(y_pos - 0.3, noramberd,width = 0.3,label = 'Nor Amberd', hatch= "\\\\", edgecolor = 'white', color='C3')
     ax1.bar(y_pos + 0.3, yerevan,width = 0.3,label = 'Yerevan', hatch= h, edgecolor = 'white', color='C1')
     ax1.tick_params(axis='both', which='major', labelsize=16)
-    ax1.set_ylabel('Averaged count of event per year', fontsize = 18)
+    ax1.set_ylabel('Averaged count of event per year', fontsize = 18, labelpad = 18)
     ax1.set_xlabel('Time(LT)', fontsize = 18)
-    ax1.text(-1, -1.2, 'a)', fontsize = 16)
+    ax1.text(-1, -1.2, 'c)', fontsize = 16)
 
 def monthlybar(mlst1,mlst2,mlst3,ax2):
     ind1 = np.arange(7)
@@ -29,15 +29,15 @@ def monthlybar(mlst1,mlst2,mlst3,ax2):
     ax2.set_ylabel('Count of Thunderstorm', fontsize = 18)
     ax2.set_xlabel('Months', fontsize = 18)
     ax2.text(-1, -15, 'b)', fontsize = 16)
-
+    ax2.legend(fontsize = 18)
 def yearlybar(lst1,lst2,lst3,ax3):
     ind = np.arange(8)
     ax3.bar(ind  , lst1 ,width = 0.3,label = 'Aragats', hatch= "-", edgecolor = 'white', color='C0')
     ax3.bar(ind - 0.3, lst2 ,width = 0.3,label = 'Nor Amberd', hatch= "\\\\", edgecolor = 'white', color='C3')
     ax3.bar(ind + 0.3, lst3 ,width = 0.3,label = 'Yerevan', hatch= h, edgecolor = 'white', color='C1')
     #ax3.legend(loc='center left', bbox_to_anchor=(1, 0.5), fontsize = 13)
-    ax3.legend(fontsize = 18)
-    #ax3.set_ylim(0,120)
+    
+    ax3.set_ylim(0,120)
     xTickMarks = [' ','2012','2013','2014','2015','2016','2017','2018','2019']
     xtickNames = ax3.set_xticklabels(xTickMarks)
     plt.setp(xtickNames, fontsize = 20)
@@ -46,5 +46,5 @@ def yearlybar(lst1,lst2,lst3,ax3):
     #ax3.set_title('Averaged max wind speed during thunderstorms (2012-2019)', fontsize = 22)
     #ax3.set_ylabel('Averaged wind speed(m/s)', fontsize = 20)
     ax3.set_xlabel('Years', fontsize = 20)
-    ax3.text(-1, -15, 'c)', fontsize = 18)
+    ax3.text(-1, -15, 'a)', fontsize = 18)
 
